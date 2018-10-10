e4891a20fd526448d8eaa3411e4b6876e8c15a73
Morgan Nicol <morgan@jump-biz.com>
2018-10-10 14:26:53 +0200

    Commit init !

---
+++ test-api.js
@@ -0,0 +1,48 @@
+//L'application requiert l'utilisation du module Express.
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
+var express = require('express');
+
+// Nous définissons ici les paramètres du serveur.
+var hostname = 'localhost';
+var port = 4620;
+
+var app = express();
+
+//Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
+//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes.
+var myRouter = express.Router();
+
+// Je vous rappelle notre route (/canada).
+myRouter.route('/canada')
+// J'implémente les méthodes GET, PUT, UPDATE et DELETE
+// GET
+    .get(function(req,res){
+        res.json({message : "Liste toutes les photos du Canada", methode : req.method});
+    })
+    //POST
+    .post(function(req,res){
+        res.json({message : "Ajoute une nouvelle photo à la liste", methode : req.method});
+    })
+    //PUT
+    .put(function(req,res){
+        res.json({message : "Mise à jour des informations des photos", methode : req.method});
+    })
+    //DELETE
+    .delete(function(req,res){
+        res.json({message : "Suppression d'une photo dans la liste", methode : req.method});
+    });
+
+myRouter.route('/')
+// all permet de prendre en charge toutes les méthodes.
+    .all(function(req,res){
+        res.json({message : "API de moi, bonjour ☺ ", methode : req.method});
+    });
+
+
+// Nous demandons à l'application d'utiliser notre routeur
+app.use(myRouter);
+
+// Démarrer le serveur
+app.listen(port, hostname, function(){
+    console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port);
+});
