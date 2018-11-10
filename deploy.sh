449f80b5b448d772d006ec602206bcdf8bde4661
ichoui <nicol.morgan@outlook.fr>
2018-11-10 13:46:32 +0100

    Kick du fichier deploy

--- deploy.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-npm run buildcss
-rsync -avz --checksum --exclude=node_modules/ --exclude=.git/  --exclude=deploy.sh --exclude=.idea --exclude=.gitignore --exclude=*.iml --exclude=package.json --exclude=package-lock.json --exclude=public/resources/scss/ --exclude=images/banff/* ./ root@192.168.1.84:/var/www/API/
