f74c1f72e67e6a490e1e45499d36ff9a9c748319
Morgan <nicol.morgan@outlook.fr>
2018-11-19 16:21:45 +0100

    Séparation des js

--- public/resources/js/data-json.js
+++ public/resources/js/data-json.js
@@ -133,12 +133,4 @@ $(document).ready(function () {
         slideToScroll: 1,
         infinite: false
     });
-
-    // RANDOM BACKGROUND IMAGE
-    function getRandomInt(max, min) {
-        return Math.floor(Math.random() * max) + min;
-    }
-
-    const rand = getRandomInt(4, 1);
-    $('body').addClass('body' + rand)
 });
