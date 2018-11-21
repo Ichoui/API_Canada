1cfa68b7095fc4caa4084e42cc674ea277edbcb1
ichoui <nicol.morgan@outlook.fr>
2018-11-21 21:00:52 +0100

    putaing

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const http = require('http');
 const app = require('./app');
 
-const hostname = process.env.HOSTNAME || "localhost";
-const port = process.env.HOSTNAME || 4620;
+const hostname = 'localhost';
+const port = 4620;
 
 const server = http.createServer(app);
 
